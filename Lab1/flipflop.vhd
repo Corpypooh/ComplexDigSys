797d81ce9911e8068489802a83f86137f19ea41f
gdonados <79154150+gdonados@users.noreply.github.com>
2022-02-14 15:42:49 -0500

    Created flipflop and prototyped part 1

---
+++ Lab1/flipflop.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity flipflop is
+port(
+	D, Clk, EN, RST	:	in		std_logic;
+	Q			:	out	std_logic);
+end flipflop;
+
+architecture imp of flipflop is
+begin
+	process (Clk)
+	begin
+		if rising_edge(Clk) then
+			if RST = '1' then
+				Q <= '0';
+			else
+				if EN = '1' then
+					Q <= D;
+				end if;
+		end if;
+	end process;
+end imp;
